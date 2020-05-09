046fbc97fb802bd00e984da620d65d149feea340
yushuang <yushuang@qiyi.com>
2020-05-09 10:24:14 +0800

    第36题 查看是否存在独数

---
+++ algorithm/src/main/java/ValidateDuShu36.java
@@ -0,0 +1,32 @@
+/**
+ * @author: yushuang
+ * @email: yushuang@qiyi.com
+ * @date: 2020/5/9
+ * @description:
+ **/
+public class ValidateDuShu36 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isValidSudoku(char[][] board) {
+        int[][] row = new int[9][0];
+        int[][] col = new int[9][9];
+        int[][] block = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int val = board[i][j] - '1';
+                    int blockIndex = i / 3 + j / 3 * 3;
+                    row[i][val] += 1;
+                    col[j][val] += 1;
+                    block[blockIndex][val] += 1;
+                    if (row[i][val] > 1 || col[j][val] > 1 || block[blockIndex][val] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
