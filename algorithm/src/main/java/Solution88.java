c9f5afd9b8a2ff6077a8ce93ee9d0c5796c94ec8
yushuang <yushuang@qiyi.com>
2020-06-05 20:26:56 +0800

    Solution 88

--- algorithm/src/main/java/Solution88.java
+++ algorithm/src/main/java/Solution88.java
@@ -5,13 +5,13 @@
  * @description:
  * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组
  **/
-public class Solution87 {
+public class Solution88 {
     public static void main(String[] args) {
         int[] num1 = new int[]{1,2,3,0,0,0};
         int m = 3;
         int[] num2 = new int[]{2,5,6};
         int n = 3;
-        Solution87 solution = new Solution87();
+        Solution88 solution = new Solution88();
         solution.merge(num1, m, num2, n);
         System.out.println(num1.toString());
     }
