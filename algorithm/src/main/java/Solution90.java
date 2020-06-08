534f20536b8a849ba0f0fed24475dd556771faaa
yushuang <yushuang@qiyi.com>
2020-06-08 10:25:42 +0800

    Solution 89 &  90

---
+++ algorithm/src/main/java/Solution90.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author: yushuang
+ * @email: yushuang@qiyi.com
+ * @date: 2020/6/7
+ * @description:
+ * 给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ **/
+public class Solution90 {
+
+    public static void main(String[] args) {
+        //回溯法处理
+        int[] nums = {1,2,2};
+        Solution90 solution = new Solution90();
+        List<List<Integer>> lists = solution.subsetsWithDup(nums);
+        System.out.println(lists);
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        Boolean[] visit = new Boolean[nums.length];
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        helper(lists, list, nums, visit,0);
+        return lists;
+    }
+
+    /**
+     * 排序加剪枝
+     *
+     * @param lists
+     * @param list
+     * @param nums
+     * @param index
+     */
+    private void helper(List<List<Integer>> lists, List<Integer> list, int[] nums, Boolean[] visit, int index) {
+        lists.add(new ArrayList<>(list));
+        for (int i = index; i < nums.length; i++) {
+            if (i > 0 && !visit[i - 1] && nums[i] == nums[i - 1]) continue;
+            visit[i] = true;
+            list.add(nums[i]);
+            helper(lists, list, nums, visit, index + 1);
+            list.remove(list.size() - 1);
+            visit[i] = false;
+        }
+    }
+
+}
