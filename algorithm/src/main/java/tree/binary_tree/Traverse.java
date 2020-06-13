9537e500710defcb308df9ca33beeab695aff35b
yushuang <yushuang@qiyi.com>
2020-06-13 14:05:16 +0800

    Solution94 & 95

---
+++ algorithm/src/main/java/tree/binary_tree/Traverse.java
@@ -0,0 +1,17 @@
+package tree.binary_tree;
+
+/**
+ * @author: yushuang
+ * @email: yushuang@qiyi.com
+ * @date: 2020/6/11
+ * @description:
+ **/
+public class Traverse {
+
+    //前序遍历
+
+    //中序遍历
+
+    //后续遍历
+
+}
