07a11aecbbc92acdad80032f07ab65e089fd0e0a
yushuang <yushuang@qiyi.com>
2021-03-02 09:54:38 +0800

    Solution

--- algorithm/src/main/java/classify/datastructure/Solution76.java
+++ algorithm/src/main/java/classify/datastructure/Solution76.java
@@ -7,34 +7,75 @@ package classify.datastructure;
  * @description:
  **/
 public class Solution76 {
+
+    public static void main(String[] args) {
+        Solution76 solution = new Solution76();
+        String s = "ADOBECODEBANC";
+        String c = "ABC";
+        String minWindow = solution.minWindow(s, c);
+        System.out.println(minWindow);
+    }
+
     public String minWindow(String s, String t) {
-        int start = 0;
-        int left = -1, right = -1;
-        int max = Integer.MAX_VALUE;
-        int cnt = t.length();
-        char[] sc = new char[256], tc = new char[256];
+        // 有效性判断
+        int start = 0, left = -1, right = -1;
+        int[] sc = new int[256], tc = new int[256];
+        int len = Integer.MAX_VALUE;
+        int count = t.length();
         for (int i = 0; i < t.length(); i++) {
             tc[t.charAt(i)]++;
         }
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             sc[c]++;
-            if (sc[c] <= tc[c]) cnt--;
-            while (start <= i && cnt == 0) {
-                char r = s.charAt(start);
-                sc[r]--;
-                if (sc[r] < tc[r]) cnt++;
-                if (cnt > 0) {
-                    if (max > i - start + 1) {
+            if (sc[c] <= tc[c]) count--;
+            while (count == 0 && start < i) {
+                char ch = s.charAt(start);
+                sc[ch]--;
+                if (sc[ch] < tc[ch]) count++;
+                if (count > 0) {
+                    if ((i - start + 1) < len) {
                         left = start;
                         right = i;
-                        max = i - start + 1;
+                        len = (i - start + 1);
                     }
                 }
                 start++;
             }
         }
-        if (left == -1) return "";
+        if (len == Integer.MAX_VALUE) return "";
         return s.substring(left, right + 1);
     }
+
+
+//    public String minWindow(String s, String t) {
+//        int start = 0;
+//        int left = -1, right = -1;
+//        int max = Integer.MAX_VALUE;
+//        int cnt = t.length();
+//        char[] sc = new char[256], tc = new char[256];
+//        for (int i = 0; i < t.length(); i++) {
+//            tc[t.charAt(i)]++;
+//        }
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            sc[c]++;
+//            if (sc[c] <= tc[c]) cnt--;
+//            while (start <= i && cnt == 0) {
+//                char r = s.charAt(start);
+//                sc[r]--;
+//                if (sc[r] < tc[r]) cnt++;
+//                if (cnt > 0) {
+//                    if (max > i - start + 1) {
+//                        left = start;
+//                        right = i;
+//                        max = i - start + 1;
+//                    }
+//                }
+//                start++;
+//            }
+//        }
+//        if (left == -1) return "";
+//        return s.substring(left, right + 1);
+//    }
 }
--- algorithm/src/main/java/classify/dp/Solution42.java
+++ algorithm/src/main/java/classify/dp/Solution42.java
@@ -5,33 +5,47 @@ package classify.dp;
  * @author: yushuang
  * @email: yushuang@qiyi.com
  * @date: 2020/12/27
- * @description:
- * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水；
+ * @description: 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水；
  **/
 public class Solution42 {
 
     public static void main(String[] args) {
-        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int[] height = {4, 2, 0, 3, 2, 5};
         Solution42 solution = new Solution42();
         int cap = solution.trap(height);
         System.out.println(cap);
     }
 
-    public Integer trap(int[] height) {
+//    public Integer trap(int[] height) {
+//        int cap = 0;
+//        for (int i = 1; i < height.length - 1; i++) {
+//            int left = i - 1, right = i +1;
+//            int leftMax = 0, rightMax = 0;
+//            while (left >= 0) {
+//                leftMax = height[left] > leftMax ? height[left] : leftMax;
+//                left--;
+//            }
+//            while (right <= height.length - 1) {
+//                rightMax = height[right] > rightMax ? height[right] : rightMax;
+//                right++;
+//            }
+//            if (leftMax >= height[i] && rightMax >= height[i]) {
+//                cap += Math.min(rightMax, leftMax) - height[i];
+//            }
+//        }
+//        return cap;
+//    }
+
+    public int trap(int[] height) {
+        if (null == height || height.length == 0) return 0;
         int cap = 0;
         for (int i = 1; i < height.length - 1; i++) {
-            int left = i - 1, right = i +1;
+            int left = i - 1, right = i + 1;
             int leftMax = 0, rightMax = 0;
-            while (left >= 0) {
-                leftMax = height[left] > leftMax ? height[left] : leftMax;
-                left--;
-            }
-            while (right <= height.length - 1) {
-                rightMax = height[right] > rightMax ? height[right] : rightMax;
-                right++;
-            }
-            if (leftMax >= height[i] && rightMax >= height[i]) {
-                cap += Math.min(rightMax, leftMax) - height[i];
+            for (; left >= 0; left--) leftMax = leftMax > height[left] ? leftMax : height[left];
+            for (; right <= height.length - 1; right++) rightMax = rightMax > height[right] ? rightMax : height[right];
+            if (leftMax > height[i] && rightMax > height[i]) {
+                cap += Math.min(leftMax, rightMax) - height[i];
             }
         }
         return cap;
